cf863d5f3b7dc47cc94271d50d8bfd6fcb5cbb38
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-05 20:44:31 +0200

    Resolve AboutArithmeticOperators simpleOperations

--- koans/src/beginner/AboutArithmeticOperators.java
+++ koans/src/beginner/AboutArithmeticOperators.java
@@ -9,14 +9,14 @@ public class AboutArithmeticOperators {
 
     @Koan
     public void simpleOperations() {
-        assertEquals(1, __);
-        assertEquals(1 + 1, __);
-        assertEquals(2 + 3 * 4, __);
-        assertEquals((2 + 3) * 4, __);
-        assertEquals(2 * 3 + 4, __);
-        assertEquals(2 - 3 + 4, __);
-        assertEquals(2 + 4 / 2, __);
-        assertEquals((2 + 4) / 2, __);
+        assertEquals(1, 1);
+        assertEquals(1 + 1, 2);
+        assertEquals(2 + 3 * 4, 14);
+        assertEquals((2 + 3) * 4, 20);
+        assertEquals(2 * 3 + 4, 10);
+        assertEquals(2 - 3 + 4, 3);
+        assertEquals(2 + 4 / 2, 4);
+        assertEquals((2 + 4) / 2, 3);
     }
 
     @Koan
