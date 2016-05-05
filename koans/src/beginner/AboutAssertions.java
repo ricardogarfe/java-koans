3e1b0057414004f0010cc94cb64507ebfe00e168
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-05 16:59:31 +0200

    Resolve assertEqualsWithAFewExpressions

--- koans/src/beginner/AboutAssertions.java
+++ koans/src/beginner/AboutAssertions.java
@@ -45,11 +45,11 @@ public class AboutAssertions {
     @Koan
     public void assertEqualsWithAFewExpressions() {
         assertEquals("Hello World!", __);
-        assertEquals(1, __);
-        assertEquals(2 + 2, __);
-        assertEquals(2 * 3, __);
-        assertEquals(3 - 8, __);
-        assertEquals(10 / 2, __);
+        assertEquals(1, 1);
+        assertEquals(2 + 2, 4);
+        assertEquals(2 * 3, 6);
+        assertEquals(3 - 8, -5);
+        assertEquals(10 / 2, 5);
     }
 
     @Koan
