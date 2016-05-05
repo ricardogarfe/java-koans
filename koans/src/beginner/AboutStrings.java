31485eb21e6a7f7fafd635a680f61f3ade0be495
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-05 17:27:45 +0200

    Resolve AboutStrings insufficientArgumentsToStringFormatCausesAnError

--- koans/src/beginner/AboutStrings.java
+++ koans/src/beginner/AboutStrings.java
@@ -164,8 +164,8 @@ public class AboutStrings {
             String.format("%s %s %s", "a", "b");
             fail("No Exception was thrown!");
         } catch (Exception e) {
-            assertEquals(e.getClass(), __);
-            assertEquals(e.getMessage(), __);
+            assertEquals(e.getClass(), java.util.MissingFormatArgumentException.class);
+            assertEquals(e.getMessage(), "Format specifier '%s'");
         }
     }
 
