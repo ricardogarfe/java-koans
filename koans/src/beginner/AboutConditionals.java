dbcdebb12f73c7f0719402b826661d8e5d741da5
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-09 12:32:38 +0200

    Resolve AboutConditionals switchStatementConstants

--- koans/src/beginner/AboutConditionals.java
+++ koans/src/beginner/AboutConditionals.java
@@ -125,7 +125,7 @@ public class AboutConditionals {
             default:
                 result += "Nothing";
         }
-        assertEquals(result, __);
+        assertEquals(result, "Basic Nothing");
     }
 
     @Koan
