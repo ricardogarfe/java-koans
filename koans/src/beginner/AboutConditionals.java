633fae1c7c93ef5de60113fcaf6718c4430031ea
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-09 12:30:25 +0200

    Resolve AboutConditionals switchStatementFallThrough

--- koans/src/beginner/AboutConditionals.java
+++ koans/src/beginner/AboutConditionals.java
@@ -93,7 +93,7 @@ public class AboutConditionals {
             default:
                 result += "Nothing";
         }
-        assertEquals(result, __);
+        assertEquals(result, "Basic OneTwoNothing");
     }
 
     @Koan
