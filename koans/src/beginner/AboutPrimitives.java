6322cb23b3eacfe81b04822e91dba8ad65cba365
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-11 13:35:42 +0200

    Resolve AboutPrimitives floatsHaveASmallerRangeThanDoubles

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -183,8 +183,8 @@ public class AboutPrimitives {
 
     @Koan
     public void floatsHaveASmallerRangeThanDoubles() {
-        assertEquals(Float.MIN_VALUE, __);
-        assertEquals(Float.MAX_VALUE, __);
+        assertEquals(Float.MIN_VALUE, 1.4E-45f);
+        assertEquals(Float.MAX_VALUE, 3.4028235E38f);
     }
 
     @Koan
