bd1204dfe4aa9da9e1bfaf90612a121c4eada6c6
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-11 13:32:36 +0200

    Resolve AboutPrimitives primitivesOfTypeDoubleCanBeDeclaredWithExponents

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -141,9 +141,9 @@ public class AboutPrimitives {
 
     @Koan
     public void primitivesOfTypeDoubleCanBeDeclaredWithExponents() {
-        assertEquals(getType(1e3), __);
-        assertEquals(1.0e3, __);
-        assertEquals(1E3, __);
+        assertEquals(getType(1e3), double.class);
+        assertEquals(1.0e3, double.class);
+        assertEquals(1E3, double.class);
     }
 
     @Koan
