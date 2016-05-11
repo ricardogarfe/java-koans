13c91e055dcce980571347eafed23ba63ecf9e9b
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-11 13:26:31 +0200

    Resolve AboutPrimitives shortsHaveASmallerRangeThanInts

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -67,8 +67,8 @@ public class AboutPrimitives {
 
     @Koan
     public void shortsHaveASmallerRangeThanInts() {
-        assertEquals(Short.MIN_VALUE, __);  // hint: You'll need an explicit cast
-        assertEquals(Short.MAX_VALUE, __);
+        assertEquals(Short.MIN_VALUE, (short)-32768);  // hint: You'll need an explicit cast
+        assertEquals(Short.MAX_VALUE, (short)32767);
     }
 
     @Koan
