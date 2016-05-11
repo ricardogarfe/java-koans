7bd2ce4c09242ae7f17e9dbebe6fd99c87481048
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-05-11 13:34:04 +0200

    Resolve AboutPrimitives doublesHaveALargeRange

--- koans/src/beginner/AboutPrimitives.java
+++ koans/src/beginner/AboutPrimitives.java
@@ -154,8 +154,8 @@ public class AboutPrimitives {
 
     @Koan
     public void doublesHaveALargeRange() {
-        assertEquals(Double.MIN_VALUE, __);
-        assertEquals(Double.MAX_VALUE, __);
+        assertEquals(Double.MIN_VALUE, 4.9E-324);
+        assertEquals(Double.MAX_VALUE, 1.7976931348623157E308);
     }
 
     @Koan
